336d2c74f5c8c07fc287bceee9509db2fe5acb2d
jakefurlong <jake.w.furlong@gmail.com>
2024-06-02 14:25:55 -0600

    moved all files to week1 dir

---
+++ cs50/week1/mario-right-pyramid.c
@@ -0,0 +1,34 @@
+// Create a left-justified pyramid of blocks (a la Mario Bros)
+#include <stdio.h>
+
+void print_row(int length);
+
+int main(void)
+{
+    // assign input from user to a variable height
+    int height;
+
+    do
+    {
+        printf("Enter height: ");
+        scanf("%d", &height);
+    }
+    while (height <= 0);
+    
+
+    // loop through the height and call the print_row function to produce incrementally longer rows
+    for (int j = 0; j < height; j++)
+    {
+        print_row(j + 1);
+    }
+}
+
+// generate row of blocks
+void print_row(int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
