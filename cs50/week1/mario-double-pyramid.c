00d56deed39d51bca9f8ee626aed46ccd6e4149a
jakefurlong <jake.w.furlong@gmail.com>
2024-06-02 18:35:59 -0600

    mario double pyramid

---
+++ cs50/week1/mario-double-pyramid.c
@@ -0,0 +1,47 @@
+// Create a left-justified pyramid of blocks (a la Mario Bros)
+#include <stdio.h>
+
+void print_pyramid(int height);
+
+int main(void)
+{
+    // assign input from user to a variable height
+    int height;
+
+    do
+    {
+        printf("Enter height: ");
+        scanf("%d", &height);
+    }
+    while (height <= 0);
+    print_pyramid(height);
+}
+
+// generate row of blocks
+void print_pyramid(int height)
+{
+    // build the pyramid
+    for (int i = 1; i <= height; i++)
+    {
+        // build the leading spaces
+        for (int j = 0; j < height - i; j++)
+        {
+            printf(" ");
+        }
+        // build left side blocks
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+        // add middle space
+        printf(" ");
+
+        // build right side blocks
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
